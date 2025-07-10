7b403921b35a70974e9610ab9eac66ef7b23cafb
Raymond0817 <ramon.d.santos0817@gmail.com>
2025-07-10 12:07:18 -0300

    alterado

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,23 @@
 import { Component, signal } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: false,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  dataAtual= new Date()
+  dataAtual = new Date();
+
+  constructor(private router: Router) {}
+
   hide = signal(true);
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
   }
-
+  login() {
+    this.router.navigate(['deshboard']);
+  }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,27 +1,29 @@
 import { NgModule, signal } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { LoginComponent } from './login.component';
-import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
-    CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, DatePipe
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    DatePipe,
+    FormsModule,
   ],
-  exports: [LoginComponent]
+  exports: [LoginComponent],
 })
 export class LoginModule {
-
   hide = signal(true);
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
   }
-
 }
